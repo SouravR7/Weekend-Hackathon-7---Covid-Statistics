3bcad749accc9166e6fe60bcdcb6bc1c7a854b4a
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-16 20:13:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 8080
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const { connection } = require('./connector')
+
+
+
+
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
